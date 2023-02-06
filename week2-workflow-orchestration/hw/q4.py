7a7d1d439883b9245c2c750f750dd979a679a7d2
Kyle Oda <1624udon@gmail.com>
2023-02-05 21:34:44 -0800

    q4 updates

--- week2-workflow-orchestration/hw/q4.py
+++ week2-workflow-orchestration/hw/q4.py
@@ -1,57 +1,15 @@
-from pathlib import Path
-import pandas as pd
-from prefect import flow, task
-from prefect_gcp.cloud_storage import GcsBucket
+from prefect.deployments import Deployment
+from prefect.filesystems import GitHub
+from etl_web_to_gcs import etl_web_to_gcs
 
+github_block = GitHub.load("zoom-github")
 
-@task(retries=3)
-def fetch(dataset_url: str) -> pd.DataFrame:
-    """Read data from web into pandas DataFrame"""
-
-    df = pd.read_csv(dataset_url)
-    return df
-
-
-@task(log_prints=True)
-def clean(df=pd.DataFrame) -> pd.DataFrame:
-    """Fix dtype issues"""
-    df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
-    df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
-    print(df.head(2))
-    print(f"columns: {df.dtypes}")
-    print(f"rows: {len(df)}")
-    return df
-
-
-@task()
-def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
-    """Write DataFrame out locally as a Parquet file"""
-    path = Path(f"data/{color}/{dataset_file}.parquet")
-    df.to_parquet(path, compression="gzip")
-    return path
-
-@task()
-def write_gcs(path: Path) -> None:
-    """Upload local parquet file to GCS"""
-    gcs_block = GcsBucket.load("zoom-gcs")
-    gcs_block.upload_from_path(from_path=f"{path}", to_path=path)
-    return
-
-
-@flow()
-def etl_web_to_gcs() -> None:
-    """The main ETL function"""
-    color = "green"
-    year = 2020
-    month = 11
-    dataset_file = f"{color}_tripdata_{year}-{month:02}"
-    dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
-
-    df = fetch(dataset_url)
-    df_clean = clean(df)
-    path = write_local(df_clean, color, dataset_file)
-    write_gcs(path)
-
+docker_dep = Deployment.build_from_flow(
+    flow=etl_web_to_gcs,
+    name='github-deploy',
+    storage=github_block,
+    entrypoint="week2-workflow-orchestration/hw/etl_web_to_gcs.py:etl_web_to_gcs"
+)
 
 if __name__ == "__main__":
-    etl_web_to_gcs()
+    docker_dep.apply()
